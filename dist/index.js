d0409f66596deb0fb7e711e192c3cedf83a4d2c0
foxhoundn <filip.witosz@gmail.com>
2026-01-22 09:07:27 +0100

    Removed dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-export { runCaptureCommand, makeCaptureOptions, parseViewport } from './commands/capture.js';
-export { discoverStories } from './storybook/discover.js';
-export { captureStories } from './capture/playwrightCapture.js';
-export { buildStoryUrl } from './storybook/urls.js';
-export * from './types.js';
--- dist/storybook/urls.js
+++
@@ -1,6 +0,0 @@
-const normalizeBase = (baseUrl) => baseUrl.replace(/\/$/, '');
-export const buildStoryUrl = (baseUrl, storyId) => {
-    const normalized = normalizeBase(baseUrl);
-    const encoded = encodeURIComponent(storyId);
-    return `${normalized}/iframe.html?id=${encoded}`;
-};
